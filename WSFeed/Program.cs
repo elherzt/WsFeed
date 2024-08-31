c845b971ede098b22ac0f210721af1d29625e760
Jesus Romero <jesus.romero@surespot.com>
2024-08-31 17:55:33 -0500

    add DataLayer project

--- WSFeed/Program.cs
+++ WSFeed/Program.cs
@@ -1,6 +1,13 @@
+using DataLayer;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//db reference
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=DBWsFeeed.db"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
