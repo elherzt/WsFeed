c5ee4ca35862c68946d3d291784ec81582a762ab
Jesus Romero <jesus.romero@surespot.com>
2024-08-31 20:40:45 -0500

    advances

---
+++ WSFeed/Common/Response.cs
@@ -0,0 +1,31 @@
+﻿namespace WSFeed.Common
+{
+    public class Response
+    {
+        public Response()
+        {
+           
+        }
+
+        public Response(TypeOfResponse typeOfResponse, string message = "")
+        {
+            this.TypeOfResponse = typeOfResponse;
+            this.Message = message;
+            this.Data = null;
+        }
+
+
+        public TypeOfResponse TypeOfResponse { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+
+    public enum TypeOfResponse
+    {
+        OK = 0,
+        FailedResponse = 1,
+        Exception = 2,
+        TimeOut = 3,
+        NotFound = 4,
+    }
+}
---
+++ WSFeed/Controllers/UsersController.cs
@@ -0,0 +1,66 @@
+﻿using DataLayer;
+using DataLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WSFeed.Common;
+
+namespace WSFeed.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<ActionResult<Response>> Register(UserDto userDto)
+        {
+
+            Response response = new Response(TypeOfResponse.OK);
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                response.TypeOfResponse = TypeOfResponse.Exception;
+                response.Message = ex.Message;
+            }
+            return Ok(response);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new Response
+                {
+                    ResponseType = TypeOfResponse.FailedResponse,
+                    Message = "Invalid data provided.",
+                    Data = null
+                });
+            }
+
+            var user = new User
+            {
+                Name = userDto.Name,
+                Email = userDto.Email,
+                PasswordHash = _passwordHasher.HashPassword(null, userDto.Password), // Cifrar contraseña
+                DateRegistered = DateTime.UtcNow
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(new Response
+            {
+                ResponseType = TypeOfResponse.OK,
+                Message = "User registered successfully.",
+                Data = user
+            });
+        }
+    }
+}
